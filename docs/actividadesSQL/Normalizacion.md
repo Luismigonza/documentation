1418660c447182e8701b39fc05d4c966cb3a6748
Luismigonza <luismigonzalez38@gmail.com>
2026-02-17 21:24:26 -0500

    feat:documentacion de normalizacion inicio

---
+++ docs/actividadesSQL/Normalizacion.md
@@ -0,0 +1,16 @@
+# normalizacion
+
+Aquí veremos a continuacion el procedimiento para normalizar tablas en MySQL Workbench
+
+## Tabla #1
+
+Esta es la tabla para aplicar la **Normalizacion**.\
+Como podemos ver en la tabla tiene datos los cuales estan en una **sola columna** cuando podria dividirse para que sea mas organizada. 
+
+![Alt](../../img/tablaExel1.png)
+
+## Normalizacion de la tabla #1
+
+
+
+![Alt](../../img/imagen-tabla1.png)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,15 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <Heading as="h1" className="hero__title">
-          {siteConfig.title}
+          Luis Miguel Gonzalez Berrio
+          {/* {siteConfig.title} */}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">Toda la documentacion al día{/*siteConfig.tagline*/}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ver documentacion 
           </Link>
         </div>
       </div>
